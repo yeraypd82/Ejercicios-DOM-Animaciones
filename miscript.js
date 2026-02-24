af496e52068c21b0335f19846d317f8cac21e763
yeraypd82 <yeraypd82@educastur.es>
2026-02-24 20:43:30 +0100

    Menu01
    Menu01

--- miscript.js
+++ miscript.js
@@ -1,20 +1,10 @@
-window.onload=function(){
-    let btn = document.getElementById("boton");
-    btn.addEventListener('click',mover);
-};
+window.onload = function(){
 
-function mover(){
-    let elem = document.getElementById("animar");
-    let pos = 0;
-    let id = setInterval(frame,5);
-    
-    function frame(){
-        if(pos==350){
-            clearInterval(id);
-        }else{
-        pos++;
-        elem.style.top = pos + 'px';
-        elem.style.left = pos + 'px';
-        }
-    }
-}
+    let boton = document.getElementById("btnMenu");
+    let lista = document.getElementById("listaMenu");
+
+    boton.addEventListener("click", function(){
+        lista.classList.toggle("oculto");
+    });
+
+};
